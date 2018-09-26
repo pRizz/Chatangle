a661316f7ff82eb558c2c7b0db107c43a535ce23
Peter Ryszkiewicz <pryszkie@gmail.com>
2018-09-26 07:40:57 -0500

    Added a tweet link and a deployment link

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,27 @@
             <div class="tile is-2 is-parent">
               <div class="tile is-child">
                 <channel-list :channel-controller="channelController"></channel-list>
-              </div>
+                <br/>
+                <div class="panel">
+                  <p class="panel-heading">
+                    Liking Chatangle?
+                  </p>
+
+                  <div class="panel-block">
+                    <!-- Check out Chatangle! A free, decentralized, global chatroom, powered by the IOTA tangle! #Chatangle @pryszkie https://www.chatangle.com/ -->
+                      <a class="button is-primary" style="background-color: #1da1f2;" href="http://twitter.com/home?status=Check%20out%20Chatangle!%20A%20free%2C%20decentralized%2C%20global%20chatroom%2C%20powered%20by%20the%20IOTA%20tangle!%20%23Chatangle%20%40pryszkie%20https%3A%2F%2Fwww.chatangle.com%2F
+" target="_blank">
+                        <i class="fa fa-twitter"></i>&nbsp;Spread the word!
+                      </a>
+                  </div>
+                  <div class="panel-block">
+                      <a class="button is-primary" href="https://www.npmjs.com/package/chatangle-full-stack" target="_blank">
+                        <i class="fa fa-wifi"></i>&nbsp;Deploy your own!
+                      </a>
+                  </div>
+                </div>
+
+                </div>
             </div>
 
             <div class="tile is-parent is-8 is-vertical">
